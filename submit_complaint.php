ef0b7e37bc8d9f288532c52fc97fb3ed083254ee
shahdhruv18 <155808825+shahdhruv18@users.noreply.github.com>
2024-01-06 15:27:58 +0530

    Update submit_complaint.php

--- submit_complaint.php
+++ submit_complaint.php
@@ -1,15 +1,8 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $complaintType = $_POST["cmp"];
-    $mobile = $_POST["mobile"];
-    $address = $_POST["add"];
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "formdetails";
 
-    $data = "Name: $name\nEmail: $email\nComplaint Type: $complaintType\nMobile: $mobile\nAddress: $address\n\n";
-    file_put_contents("complaints.txt", $data, FILE_APPEND);
-    header("Location: thank_you.php");
-    exit();
-}
+  $conn=
 ?>
