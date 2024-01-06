d804e5f52db37792c78d1c7855998e92c2f983c7
shahdhruv18 <155808825+shahdhruv18@users.noreply.github.com>
2024-01-06 15:44:38 +0530

    Update submit_complaint.php

--- submit_complaint.php
+++ submit_complaint.php
@@ -1,8 +1,33 @@
 <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "formdetails";
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'dbname';
 
-  $conn=
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $complaint = $_POST['cmp'];
+    $mobile = $_POST['mobile'];
+    $address = $_POST['address'];
+
+    
+    $sql = "INSERT INTO forndetails (name, email, complaint, mobile, address) VALUES ('$name', '$email', '$complaint', '$mobile', '$address')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Complaint submitted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close the database connection
+$conn->close();
 ?>
